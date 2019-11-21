dfbc6ce3844e5b9ce8bf52fcf8e7d79966210ffd
Alex Von Hoene <avh.on1@gmail.com>
2019-11-21 09:37:44 -0500

    Create conditional2.js

---
+++ conditional2.js
@@ -0,0 +1,10 @@
+const bowlIsEmpty = false;
+const dogNeedsBrushies = true;
+if ( bowlIsEmpty ) {
+	alert("Fill his bowl!");
+} else if ( dogNeedsBrushies ) {
+	alert("Brush his fur!");
+} else { 
+	alert("Walk the dog!");
+}
+alert("Good job!");
